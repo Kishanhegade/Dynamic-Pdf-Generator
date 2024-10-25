e784178568330329b9462a0866810008a3e9de01
KishanHegade <kishanhegade@gmail.com>
2024-10-25 11:34:36 +0530

    Add InvoiceResponse class

--- src/main/java/com/freightfox/dpg/controller/PdfController.java
+++ src/main/java/com/freightfox/dpg/controller/PdfController.java
@@ -1,6 +1,7 @@
 package com.freightfox.dpg.controller;
 
 import com.freightfox.dpg.service.PdfService;
+import com.freightfox.dpg.utility.AppResponseBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class PdfController {
     private PdfService pdfService;
+    private AppResponseBuilder builder;
+
 
 
 }
---
+++ src/main/java/com/freightfox/dpg/dto/InvoiceResponse.java
@@ -0,0 +1,12 @@
+package com.freightfox.dpg.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InvoiceResponse {
+    String message;
+    String filepath;
+    String filename;
+}
